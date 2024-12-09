b46ebd8c7ec95365e3397fdc449178b339b09263
Nadine <nmager265@gmail.com>
2024-12-09 14:25:37 +0100

    day9 start

--- AdventofCode2024/AdventOfCode2024/AoC_24_d5_2/obj/Debug/net8.0/AoC_24_d5_2.AssemblyInfo.cs
+++ AdventofCode2024/AdventOfCode2024/AoC_24_d5_2/obj/Debug/net8.0/AoC_24_d5_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AoC_24_d5_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91216f826015b7b17c4bd99229baddbd6597ea03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66616af81756482c7358643ae66196241275a8a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AoC_24_d5_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AoC_24_d5_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ AdventofCode2024/AdventOfCode2024/AoC_24_d9_1/Program.cs
@@ -0,0 +1,48 @@
+﻿
+string input = File.ReadAllText("input_test.txt");
+
+// 2333133121414131402
+// 00...111...2...333.44.5555.6666.777.888899
+
+string translated = string.Empty;
+
+for(int i = 0; i < input.Length; i++)
+{
+    int counter = 0;
+    if (i % 2 == 0)
+    {
+        int num = input[i] - '0';
+        for(int j = 0; j < num; j++)
+        {
+            translated = translated + counter;
+        }
+        counter++;
+    }
+    else
+    {
+        int num = input[i] - '0';
+        for (int j = 0; j < num; j++)
+        {
+            translated = translated + '.';
+        }
+    }
+}
+
+char[] blocks = translated.ToCharArray();
+
+for(int i = blocks.Length -1; i >= 0; i--)
+{
+    if (blocks[i] != '.')
+    {
+        for(int j = 0; j < blocks.Length; j++)
+        {
+            if (blocks[j] == '.')
+            {
+                
+            }
+        }
+    }
+}
+
+
+
